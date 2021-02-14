3585619c393e45a169d9f1681ca179bd169ca383
Tulio Ferreira <tuliof210@gmail.com>
2021-02-14 17:59:33 -0300

    import react route

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 // styles
 import './Content.scss';
+// my components
+import Home from '../../views/home/Home';
+import Register from '../../views/register/Register';
+import Edit from '../../views/edit/Edit';
+import Employees from '../../views/employees/Employees';
 
-const Content = props => <div className="content-wrapper">Content Wrapper</div>;
+const Content = props => (
+  <div className="content-wrapper">
+    <Switch>
+      <Route path="/"></Route>
+      <Route path="/employees"></Route>
+      <Route path="/register"></Route>
+      <Route path="/edit"></Route>
+    </Switch>
+  </div>
+);
 
 export default Content;
--- src/views/remove/Remove.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-//styles
-import './Remove.scss';
-
-const Remove = props => <div className="remove-wrapper">Remove App</div>;
-
-export default Remove;
